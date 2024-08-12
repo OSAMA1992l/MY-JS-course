bd32a82ce8533497c28e5b1990c50679d4afdb31
osama <ok4476823@gmail.com>
2024-08-12 16:06:13 +0530

    added Array and object

---
+++ condistanals/Switch-condistionals.js
@@ -0,0 +1,20 @@
+// thi sis switch consitionals
+
+// let type = "subtarction"
+
+// switch(type){
+//     case "addition":
+//         console.log(10+10);
+//         break;
+//         case "subtarction":
+//             console.log(20-10);
+    
+// }
+
+
+let type = "add"
+
+switch (type){
+    case "add":
+    console.log(10+10)
+}
--- condistanals/else-if.js
+++ condistanals/else-if.js
@@ -12,17 +12,33 @@
 
 
 
-let userSubuscribtion = "power";
+// let userSubuscribtion = "power";
 
-if(userSubuscribtion == "unsubuscribed"){
-    console.log("show the pay ment page");
-}
-else if (userSubuscribtion == "power"){
-    console.log("show the hindi content");
-}
-else if(userSubuscribtion == "super"){
-    console.log("show the hollywodd movies");
-}
-else{
-    console.log("show the all movies");
-}
+// if(userSubuscribtion == "unsubuscribed"){
+//     console.log("show the pay ment page");
+// }
+// else if (userSubuscribtion == "power"){
+//     console.log("show the hindi content");
+// }
+// else if(userSubuscribtion == "super"){
+//     console.log("show the hollywodd movies");
+// }
+// else{
+//     console.log("show the all movies");
+// }
+
+// anothe questions
+
+/* age = 18 give the vote
+  age = 50 citizen
+  age 100 its senior citizen */
+
+let age = 40;
+
+  if (age<=20){
+    console.log("give the vote")
+  }
+ 
+  else if (age>=21 && age <= 50){
+    console.log("this is citizen")
+  }
